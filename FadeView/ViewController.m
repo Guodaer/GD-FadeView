8ef7cfb8be1133e8c6b26a1d016b9a055165f1de
guoda <1023251767@qq.com>
2015-11-13 12:27:45 +0800

    Fade
    最主要的

--- FadeView/ViewController.m
+++ FadeView/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "GDFadeView.h"
 @interface ViewController ()
 
 @end
@@ -17,6 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    GDFadeView *iphoneFade = [[GDFadeView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 40)];
+    iphoneFade.text = @"达哥好像很厉害似的,你说是不是";
+    iphoneFade.foreColor = [UIColor whiteColor];
+    iphoneFade.backColor = [UIColor redColor];
+    iphoneFade.font = [UIFont systemFontOfSize:25];
+    iphoneFade.alignment = NSTextAlignmentCenter;
+    iphoneFade.center = CGPointMake(self.view.bounds.size.width/2.0, self.view.bounds.size.height/2.0+50);
+    [self.view addSubview:iphoneFade];
+    [iphoneFade iPhoneFadeWithDuration:2];
+
 }
 
 - (void)didReceiveMemoryWarning {
