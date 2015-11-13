1ceacf92b969f03c48f613d2fe1dab0d6d353f53
guoda <1023251767@qq.com>
2015-11-13 11:56:38 +0800

    Initial Commit

---
+++ FadeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FadeView
+//
+//  Created by xiaoyu on 15/11/13.
+//  Copyright © 2015年 guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FadeView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FadeView
+//
+//  Created by xiaoyu on 15/11/13.
+//  Copyright © 2015年 guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
